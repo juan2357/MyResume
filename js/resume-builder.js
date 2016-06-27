ae86b8fad0680aa1c3b87f8e7703dbc9847eea52
juanfperez <juanfperez@me.com>
2016-06-27 12:36:25 -0400

    add name and role to html

--- js/resume-builder.js
+++ js/resume-builder.js
@@ -1,12 +1,20 @@
-$('#main').append(" by: Juan Perez ");
+$( document ).ready(function() {
+  // $('name').append(" by: Juan Perez ");
+  //
+  // var awesomeThoughts = "I am Juan and I am AWESOME!"
+  // console.log(awesomeThoughts);
+  //
+  // var email = "juanperez@icloud.com";
+  // var newEmail = email.replace("icloud", "me");
+  // console.log(email);
+  // console.log(newEmail);
+  //
+  // var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+  // $("#main").append(funThoughts);
 
-var awesomeThoughts = "I am Juan and I am AWESOME!"
-console.log(awesomeThoughts);
+  var formattedName = "Juan Perez";
+  var formattedRole = "Full Stack Web Developer";
+  $("#name").append(formattedName);
+  $("#role").append(formattedRole);
 
-var email = "juanperez@icloud.com";
-var newEmail = email.replace("icloud", "me");
-console.log(email);
-console.log(newEmail);
-
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+});
