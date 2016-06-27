61ee283239c2e9bb1a13c623d5590243c3f8beec
juanfperez <juanfperez@me.com>
2016-06-27 11:52:59 -0400

    added helper.js

--- js/resume-builder.js
+++ js/resume-builder.js
@@ -1,14 +1,12 @@
-$( document ).ready(function() {
-   $('#main').append(" by: Juan Perez ");
+$('#main').append(" by: Juan Perez ");
 
-   var awesomeThoughts = "I am Juan and I am AWESOME!"
-   console.log(awesomeThoughts);
+var awesomeThoughts = "I am Juan and I am AWESOME!"
+console.log(awesomeThoughts);
 
-   var email = "juanperez@icloud.com";
-   var newEmail = email.replace("icloud", "me");
-   console.log(email);
-   console.log(newEmail);
+var email = "juanperez@icloud.com";
+var newEmail = email.replace("icloud", "me");
+console.log(email);
+console.log(newEmail);
 
-  var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-  $("#main").append(funThoughts);
-});
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
